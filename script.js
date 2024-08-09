5c87de52ef8582e34faefa87ce86d3913c03aa28
ParsannaK <prasanna.koirala302@gmail.com>
2024-08-09 13:17:52 -0400

    feat: Add quiz functionality to index.html and style.css

---
+++ script.js
@@ -0,0 +1,122 @@
+const questions = [
+    {
+        question: "What is the capital of France?",
+        answers: [
+            { text: "New York", correct: false },
+            { text: "London", correct: false },
+            { text: "Paris", correct: true },
+            { text: "Dublin", correct: false }
+        ]
+    },
+    {
+        question: "Who is CEO of Tesla?",
+        answers: [
+            { text: "Jeff Bezos", correct: false },
+            { text: "Elon Musk", correct: true },
+            { text: "Bill Gates", correct: false },
+            { text: "Tony Stark", correct: false }
+        ]
+    },
+    {
+        question: "The iPhone was created by which company?",
+        answers: [
+            { text: "Apple", correct: true },
+            { text: "Intel", correct: false },
+            { text: "Amazon", correct: false },
+            { text: "Microsoft", correct: false }
+        ]
+    },
+    {
+        question: "How many Harry Potter books are there?",
+        answers: [
+            { text: "1", correct: false },
+            { text: "4", correct: false },
+            { text: "6", correct: false },
+            { text: "7", correct: true }
+        ]
+    }
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz(){
+    currentQuestionIndex = 0;
+    score = 0;
+    nextButton.innerHTML = "Next";
+    showQuestion();
+}
+
+function showQuestion(){
+    resetState();
+    let currentQuestion = questions[currentQuestionIndex];
+    let questionNo = currentQuestionIndex + 1;
+    questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+    currentQuestion.answers.forEach(answer => {
+        const button = document.createElement("button");
+        button.innerHTML = answer.text;
+        button.classList.add("btn");
+        answerButtons.appendChild(button);
+        if(answer.correct){
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener("click", selectAnswer);
+    });
+}
+
+function resetState(){
+    nextButton.style.display = "none";
+    while(answerButtons.firstChild){
+        answerButtons.removeChild(answerButtons.firstChild);
+    }
+}
+
+function selectAnswer(e){
+    const selectedBtn = e.target;
+    const isCorrect = selectedBtn.dataset.correct === "true";
+    if(isCorrect){
+        selectedBtn.classList.add("correct");
+        score++;
+    } else {
+        selectedBtn.classList.add("incorrect");
+    }
+
+    Array.from(answerButtons.children).forEach(button => {
+        if(button.dataset.correct === "true"){
+            button.classList.add("correct");
+        }
+        button.disabled = true;
+    });
+    nextButton.style.display = "block";
+}
+
+function showScore() {
+    resetState();
+    questionElement.innerHTML = "Your score is " + score + "/" + questions.length;
+    nextButton.innerHTML = "Restart";
+    nextButton.style.display = "block";
+}
+
+function handleNextButton() {
+    currentQuestionIndex++;
+    if(currentQuestionIndex < questions.length) {
+        showQuestion();
+    } else {
+        showScore();
+    }
+}
+
+nextButton.addEventListener("click", () => {
+    if(currentQuestionIndex < questions.length){
+        handleNextButton();
+    } else {
+        startQuiz();
+    }
+});
+
+startQuiz();
